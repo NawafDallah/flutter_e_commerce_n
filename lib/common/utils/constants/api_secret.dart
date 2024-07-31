664caa28e5c0a0a10f96757bce7184c9ddbd5099
Nawaf Dallah <134509210+NawafDallah@users.noreply.github.com>
2024-07-31 21:30:15 +0300

    Delete lib/common/utils/constants/api_secret.dart

--- lib/common/utils/constants/api_secret.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:convert';
-
-/* -- LIST OF Secret Constants used in APIs -- */
-const String imageApiLink = "http://10.0.2.2/e_commerce_n_flutter/uploads";
-const String secretAPIKey = "http://10.0.2.2/e_commerce_n_flutter";
-final String _basicAuth = 'Basic ${base64Encode(utf8.encode('nawaf:123456'))}';
-final Map<String, String> myheaders = {'authorization': _basicAuth};
